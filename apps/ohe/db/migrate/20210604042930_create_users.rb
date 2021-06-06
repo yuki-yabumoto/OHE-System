c84602d19e50f8e7a22f19211381bcfebc2dee0b
yyabumot <yyabumot@student.42tokyo.jp>
2021-06-06 10:16:49 +0900

    add migration file of User model

--- apps/ohe/db/migrate/20210604042930_create_users.rb
+++ apps/ohe/db/migrate/20210604042930_create_users.rb
@@ -3,12 +3,15 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :email, null: false
       t.string :password
-      t.string :gender
-      t.date :going_out_time
+      t.integer :gender
+      t.date :from_time
+      t.date :to_time
       t.integer :favorite_color
-      t.integer :favorite_style
+      t.integer :favorite_type
 
       t.timestamps
     end
+
+    add_index  :users, :email, unique: true
   end
 end
