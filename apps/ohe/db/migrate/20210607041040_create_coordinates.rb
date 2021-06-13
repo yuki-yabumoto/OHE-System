e99d1eb6ea684da3e39c59eed083a3dbc363d9e2
al19083 <84761917+al19083@users.noreply.github.com>
2021-06-13 17:54:02 +0900

    chuman/fix

--- apps/ohe/db/migrate/20210607041040_create_coordinates.rb
+++ apps/ohe/db/migrate/20210607041040_create_coordinates.rb
@@ -2,7 +2,7 @@ class CreateCoordinates < ActiveRecord::Migration[6.1]
   def change
     create_table :coordinates do |t|
       t.bigint :tops
-      t.bigini :bottoms
+      t.bigint :bottoms
       t.bigint :outer
       t.bigint :shoes
       t.bigint :accessory
