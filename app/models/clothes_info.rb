0288c89d957735a6d72119c46d08fcbc43c8ae61
al19111 <al19111@shibaura-it.ac.jp>
2021-06-15 12:30:44 +0900

    kyoumasu/add

---
+++ app/models/clothes_info.rb
@@ -0,0 +1,2 @@
+class ClothesInfo < ApplicationRecord
+end
---
+++ test/controllers/suggest_coordination_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SuggestCoordinationControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
