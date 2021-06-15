900663ad4f39a922944537dabed0f5b26b7f0ad1
al19083 <84761917+al19083@users.noreply.github.com>
2021-06-15 16:27:44 +0900

    chuman/show_coordinate_record

--- apps/ohe/app/controllers/coordinate_controller.rb
+++ apps/ohe/app/controllers/coordinate_controller.rb
@@ -1,10 +1,9 @@
 class CoordinateController < ApplicationController
-    def new
-        @coordinates=Coordinate.new
-    end
 
     def show
         user = User.find(params[:id])
-        @coordinates = Coordinate.where(user_id: user.id) #user_idが入力されたidと同じだった場合、coordinateモデルからとってくる
+        @coordinates = Coordinate.where(user_id: user.id) 
+        #user_idが入力されたidと同じだった場合、coordinateモデルからとってくる
     end
+
 end
