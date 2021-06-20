b913d9a30c32aed6da37febde58a0d560aafef14
yyabumot <yyabumot@student.42tokyo.jp>
2021-06-20 18:54:36 +0900

    天気情報取得のテストを追加

--- apps/ohe/spec/models/user_spec.rb
+++ apps/ohe/spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+
+  describe "天気予報の取得" do
+    example "天気が得られるかどうか" do
+      user = User.new(email: "test@example.com", place: "Tokyo", from_time: Time.now, to_time: Time.now + 1)
+      weather = user.getWeatherForecast
+      expect(weather).not_to eq nil
+    end
+  end
 end
