34d7621e6a2bb96dcf47478c9169129594a663af
Kousei Tanaka <al19099@shibaura-it.ac.jp>
2021-06-21 22:28:26 +0900

    add varidates update in user.rb

--- apps/ohe/app/models/user.rb
+++ apps/ohe/app/models/user.rb
@@ -7,7 +7,7 @@
 ##
 
 class User < ApplicationRecord
-	VALID_EMAIL_REGEX=/\A[\w+-.]+@[a-z\d-]+(.[a-z\d-]+)*.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+-.]+@[a-z\d-]+(.[a-z\d-]+)*.[a-z]+\z/i
 	VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
   validates :email, uniqueness: true
   validates_format_of :email, with: VALID_EMAIL_REGEX
