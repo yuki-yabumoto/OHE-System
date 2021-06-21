1c3d26a74064ee349bfd73884d6724dfbb3219d4
Kousei Tanaka <al19099@shibaura-it.ac.jp>
2021-06-21 17:13:05 +0900

    update sessions and users controller

--- apps/ohe/app/controllers/users_controller.rb
+++ apps/ohe/app/controllers/users_controller.rb
@@ -1,17 +1,39 @@
 ##
 ## File Name    : users_controller.rb
 ## Version      : 1.0
-## Designer     : 籔本悠紀
+## Designer     : 籔本悠紀,田中航生
 ## Date         : 2021.06.06
-## Purpose      : Userのコントローラー
+## Purpose      : Userのコントローラー,create文とupdate文の追加
 ##
 
 class UsersController < Base
   def new
     @user = User.new
   end
-
+  def create
+    @user=User.new(params[:user])
+    if @user.save
+      redirect_to :login
+    else
+      render action:"new"
+    end
+  end
   def edit
-    @user = current_user
+    @user=current_user
+  end
+  def update
+    @user=current_user
+    if @user.update(user_params)
+      redirect_to :index
+    else
+      render action:"edit"
+    end
+  end
+  private def user_params
+    params.require(:user).permit(
+      :gender, :place, :datetime,
+      :from_time, :to_time, :favorite_color,
+      :favorite_type
+      )
   end
 end
