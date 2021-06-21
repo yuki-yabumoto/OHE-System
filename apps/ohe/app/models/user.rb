16c704a71155732bbb0f26d1c727a107ece24a89
Kousei Tanaka <al19099@shibaura-it.ac.jp>
2021-06-21 22:01:42 +0900

    add varidates in user.rb

--- apps/ohe/app/models/user.rb
+++ apps/ohe/app/models/user.rb
@@ -7,4 +7,17 @@
 ##
 
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX=/\A[\w+-.]+@[a-z\d-]+(.[a-z\d-]+)*.[a-z]+\z/i
+	VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
+  validates :email, uniqueness: true
+  validates_format_of :email, with: VALID_EMAIL_REGEX
+	validates_format_of :password, with: VALID_PASSWORD_REGEX
+	validates :email, presence: true, on: :create
+	validates :password, presence: true, on: :create
+  validates :gender, presence: true, on: :update
+  validates :place, presence: true, on: :update
+  validates :from_time, presence: true, on: :update
+  validates :to_time, presence: true, on: :update
+  validates :favorite_color, presence: true, on: :update
+  validates :favorite_type, presence: true, on: :update
 end
