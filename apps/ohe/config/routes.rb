08eaddbbb52cc1958e884e35dd45cf15b0838699
yyabumot <yyabumot@student.42tokyo.jp>
2021-06-26 19:44:28 +0900

    fix routing

--- apps/ohe/config/routes.rb
+++ apps/ohe/config/routes.rb
@@ -1,12 +1,13 @@
 ##
 ## File Name    : routes.rb
-## Version      : 1.0
+## Version      : v1.0
 ## Designer     : 籔本悠紀
 ## Purpose      : アプリケーションのルーティングを定義する．
 ##
 
 Rails.application.routes.draw do
   root "top#index", as: :index
+  get "login" => "sessions#new", as: :login
   resource :session, only: [ :new, :create, :destroy ]
   resource :users
   resources :clothes
