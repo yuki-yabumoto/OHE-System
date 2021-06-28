91dfbe7ae6f9386252b6cc0e6b6c55c72d5f66be
Nakamori Fuuta <al19097@shibaura-it.ac.jp>
2021-06-28 16:27:15 +0900

    nakamori/add spec login and new register

--- apps/ohe/spec/requests/users_spec.rb
+++ apps/ohe/spec/requests/users_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /users/new" do
+    it '新規登録画面の表示に成功すること' do
+      get '/users/new'
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "POST /users/new" do
+    it '新規登録に成功すること' do
+      post users_path, params: { user: { email: "test@e.com", password: "1234"} }
+      expect(response).to have_http_status(302)
+    end
   end
 end
