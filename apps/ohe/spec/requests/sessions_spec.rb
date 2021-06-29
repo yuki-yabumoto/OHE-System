797d3c3ee136b4c3fc39369d2356b92eee769113
Nakamori Fuuta <al19097@shibaura-it.ac.jp>
2021-06-29 20:05:02 +0900

    nakamori/add spec of login and register

--- apps/ohe/spec/requests/sessions_spec.rb
+++ apps/ohe/spec/requests/sessions_spec.rb
@@ -1,7 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /login" do
+    it 'ログイン画面の表示に成功すること' do
+      get "/login"
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "POST /login" do
+    context 'ユーザーが存在する場合' do
+      it 'パスワードが正しくログインに成功すること' do
+        post session_path, params: { login_form: { email: "test@a.com", password: "1234"} }
+        expect(response).to have_http_status(200)
+      end
+      it 'パスワードが異なりログインに失敗すること' do
+        post session_path, params: { login_form: { email: "test@a.com", password: "abcd"} }
+        expect(flash.alert).not_to be nil
+      end
+    end
+
+    context 'ユーザーが存在しない場合' do
+      it 'ログインに失敗すること' do
+        post session_path, params: { login_form: { email: "test@b.com", password: "1234"} }
+        expect(flash.alert).not_to be nil
+      end
+    end
   end
 end
