d7ed09665ac073e97d2a82a3f50e8cecad1ac491
Yoshiki <al19014@shibaura-it.ac.jp>
2021-06-30 23:10:26 +0900

    new

--- apps/ohe/app/controllers/clothes_controller.rb
+++ apps/ohe/app/controllers/clothes_controller.rb
@@ -1,4 +1,3 @@
-##
 ## File Name    : clothes_controller.rb
 ## Version      : v2.0
 ## Designer     : 籔本悠紀
@@ -7,27 +6,23 @@
 ##
 
 class ClothesController < Base
-
   def index
     user = current_user
     @clothes = Clothe.where(user_id: user.id)
     render action: "index"
   end
 
-
   def new
     @clothe = Clothe.new
   end
 
   def show
-
     clothe = Clothe.find(params[:id])
     redirect_to [ :edit, clothe ]
   end
 
   def edit
     @clothe = Clothe.find(params[:id]);
-
   end
 
   def create
@@ -36,12 +31,10 @@ class ClothesController < Base
     param['user_id'] = user.id
     @clothe = Clothe.new(param)
     if @clothe.save
-
       flash.notice = "服を追加しました"
       redirect_to :clothes
     else
       flash.alert = "服の追加に失敗しました"
-
       render action:"new"
     end
   end
@@ -50,8 +43,6 @@ class ClothesController < Base
     clothe = Clothe.find(params[:id])
     clothe.destroy!
     flash.notice = "服を削除しました"
-
     redirect_to :clothes
-
   end
 end
