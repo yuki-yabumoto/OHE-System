ba1c20c53cc3720bd0c01085df63200ed8af3399
yyabumot <yyabumot@student.42tokyo.jp>
2021-07-03 21:47:28 +0900

    Coordinateモデルにuser_idを追加

--- apps/ohe/db/migrate/20210607041040_create_coordinates.rb
+++ apps/ohe/db/migrate/20210607041040_create_coordinates.rb
@@ -1,6 +1,7 @@
 class CreateCoordinates < ActiveRecord::Migration[6.1]
   def change
     create_table :coordinates do |t|
+      t.bigint :user_id
       t.bigint :tops
       t.bigint :bottoms
       t.bigint :outer
