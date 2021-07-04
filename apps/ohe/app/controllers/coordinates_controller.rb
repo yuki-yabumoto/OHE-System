028b109ce99ce8ff7adc73ff56436d9b1802dfdb
yyabumot <yyabumot@student.42tokyo.jp>
2021-07-04 09:57:55 +0900

    CoordinatesControllerの修正

--- apps/ohe/app/controllers/coordinates_controller.rb
+++ apps/ohe/app/controllers/coordinates_controller.rb
@@ -16,19 +16,20 @@ class CoordinatesController < Base
     end
 
     def new
-        @coordinate = Coordinate.new
         user = current_user
-        weather = user.getWeatherForecast
-        @coordinate = @coordinate.suggest(weather[:min_temperature], weather[:max_humidity] \
-                                , weather[:weather], user.id)
-        if !@e1 && !@e2
+        @weather = user.getWeatherForecast
+
+        @coordinate = Coordinate.new
+        @coordinate = @coordinate.suggest(@weather[:min_temperature], @weather[:max_humidity] \
+                                , @weather[:weather], user.id)
+        if !@coordinate.kind_of?(String)
             param = {}
             param['user_id'] = user.id
             param['tops'] = @coordinate.tops != nil ? @coordinate.tops.id : nil
             param['bottoms'] = @coordinate.bottoms != nil ?  @coordinate.bottoms.id : nil
             param['outer'] = @coordinate.outer != nil ? @coordinate.outer.id : nil
             param['shoes'] = @coordinate.shoes != nil ? @coordinate.shoes.id : nil
-            param['accessory'] = @coordinate.accesary.id != nil ? @coordinate.accesary.id : nil
+            param['accessory'] = @coordinate.accessory.id != nil ? @coordinate.accessory.id : nil
             c = Coordinate.new(param)
             c.save
         end
