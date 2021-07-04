cf2aa59564e3b8d5c96dc033731d36a06a4918ad
yyabumot <yyabumot@student.42tokyo.jp>
2021-07-04 19:19:18 +0900

    CoordinatesControllerをアップデート

--- apps/ohe/app/controllers/coordinates_controller.rb
+++ apps/ohe/app/controllers/coordinates_controller.rb
@@ -12,17 +12,25 @@ class CoordinatesController < Base
         user = current_user
         @coordinates = Coordinate.where(user_id: user.id)
         @date = Coordinate.select("created_at")
-        render action: "index"
     end
 
     def new
+    end
+
+    def create
+        # 天気予報を取得
         user = current_user
+        user.from_time = DateTime.parse("#{params["from(1i)"]}-#{params["from(2i)"]}-#{params["from(3i)"]}T#{params["from(4i)"]}:#{params["from(5i)"]}")
+        user.to_time = DateTime.parse("#{params["to(1i)"]}-#{params["to(2i)"]}-#{params["to(3i)"]}T#{params["to(4i)"]}:#{params["to(5i)"]}")
+        user.save
         @weather = user.getWeatherForecast
 
+        # コーディネートを取得
         @coordinate = Coordinate.new
         @coordinate = @coordinate.suggest(@weather[:min_temperature], @weather[:max_humidity] \
                                 , @weather[:weather], user.id)
         if !@coordinate.kind_of?(String)
+            # エラーが起きてないとき，コーディネートをDBに保存
             param = {}
             param['user_id'] = user.id
             param['tops'] = @coordinate.tops != nil ? @coordinate.tops.id : nil
