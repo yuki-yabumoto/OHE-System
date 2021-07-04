04fb72914db65ec35b0f781cfd16ffdca994642a
yyabumot <yyabumot@student.42tokyo.jp>
2021-07-04 22:49:23 +0900

    clotheモデルのspecを追加

--- apps/ohe/spec/models/clothe_spec.rb
+++ apps/ohe/spec/models/clothe_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe Clothe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "Clothe",type: :request do
+  describe 'POST #create' do
+    it '服の登録に成功すること' do
+      post clothe_path, params: { clothe: { kind:"1", color:"1", type:"1",image:"null"} }
+      expect(response).to have_http_status(200)
+    end
+  end
 end
